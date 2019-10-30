d670199d361767fad55ed19c6bd21358d0fd0513
Marco Baroncini <spartaco4404@yahoo.it>
2019-10-30 11:02:45 +0100

    addedd products variations in season model, addedd 'id' parameter to route object

--- routes/route.php
+++ routes/route.php
@@ -38,33 +38,69 @@ add_action( 'rest_api_init', function () {
             if ( isset($route['acf']['product']) && is_array($route['acf']['product']) )
             {
                 $found_products = $route['acf']['product'];
-                $products = [];
-                foreach ( $found_products as $key => $product_id )
-                {
-                    $product = wc_get_product($product_id)->get_data();
 
-                    $handle = new WC_Product_Variable($product_id);
-                    $variations_ids = $handle->get_children();
-                    if ( ! empty ($variations_ids) )
-                    {
-                        $variations = [];
-                        foreach( $variations_ids as $variations_id)
-                        {
-                            $single_variation = new WC_Product_Variation($variations_id);
-                            $variations[] = $single_variation->get_data();
-                        }
-                        $product['variations'] = $variations;
-
-                    }
-
-                    $products[ $product_id ] = $product;
-
-                }
-                $route['acf']['product'] = $products;
+                $route['acf']['product'] = webmapp_vn_route_api_format_products( $found_products );
             }
+
+
+            if ( isset($route['acf']['model_season']) && is_array($route['acf']['model_season']) )
+            {
+                $model_season = $route['acf']['model_season'];
+                foreach ( $model_season as $key => $val )
+                {
+                    if ( isset($val['product']) && is_array($val['product']) )
+                    {
+                        $model_season[$key]['product'] = webmapp_vn_route_api_format_products($val['product']);
+                    }
+                }
+
+            }
+            
+
+            $route = webmapp_vn_route_api_route_object_customization($route);
 
 
             return new WP_REST_Response( $route, 200 );
         },
     ) );
 } );
+
+
+
+function webmapp_vn_route_api_format_products( $found_products )
+{
+    $products = [];
+    foreach ( $found_products as $key => $product_id )
+    {
+        $product = wc_get_product($product_id)->get_data();
+
+        $handle = new WC_Product_Variable($product_id);
+        $variations_ids = $handle->get_children();
+        if ( ! empty ($variations_ids) )
+        {
+            $variations = [];
+            foreach( $variations_ids as $variations_id)
+            {
+                $single_variation = new WC_Product_Variation($variations_id);
+                $variations[] = $single_variation->get_data();
+            }
+            $product['variations'] = $variations;
+
+        }
+
+        $products[ $product_id ] = $product;
+
+    }
+
+    return $products;
+}
+
+
+function webmapp_vn_route_api_route_object_customization( $route )
+{
+    $route_id = $route['ID'];
+    $route['id'] = $route_id;
+    //unset( $route['ID'] );
+
+    return $route;
+}
